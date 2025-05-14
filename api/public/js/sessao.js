d6821da5cc33a0749f9e657ea09f8370897dbdf6
EduardoNdL <eduardo.lima@sptech.school>
2025-05-14 20:02:05 -0300

    att 1

--- api/public/js/sessao.js
+++ api/public/js/sessao.js
@@ -2,10 +2,9 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
     var b_usuario = document.getElementById("b_usuario");
 
-    if (email != null && nome != null && logou != null) {
+    if (email != null && nome != null && logou != null && sessionStorage.Logou == "Sim") {
         b_usuario.innerHTML = nome;
     } else {
         window.location = "../login.html";
